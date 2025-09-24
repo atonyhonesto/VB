0a68f1300dbe5c621cd4d89e4e887888551a386d
atonyhonesto <atonyhonesto@gmail.com>
2025-09-24 19:51:45 -0400

    Update SqlServerCrud.vb

--- VB CRUD Helper/SqlServerCrud.vb
+++ VB CRUD Helper/SqlServerCrud.vb
@@ -1,1 +1,27 @@
 
+# SqlServerCrud (VB.NET)
+
+A lightweight, dependency-free ADO.NET helper for SQL Server with generic CRUD, stored procedures, paging, and retry logic. Written in **Visual Basic**.
+
+## Features
+- Generic CRUD with safe parameterization
+- Sync & Async APIs
+- Optional retry (deadlocks, timeouts)
+- Stored procedure helpers
+- Paging (OFFSET/FETCH)
+- Transaction wrapper
+
+## Quick Start
+```vb
+Dim db = New SqlServerCrud("Server=.;Database=MyDb;Trusted_Connection=True;")
+
+Dim newId As Long = db.InsertAndReturnIdentity(Of Long)(
+    "Users",
+    New Dictionary(Of String, Object) From {
+        {"FirstName","Tony"},
+        {"LastName","Honesto"},
+        {"Email","atonyhonesto@gmail.com"}
+    },
+    identityColumnName:="UserId")
+
+Dim row = db.GetById("Users", New Dictionary(Of String, Object) From {{"UserId", newId}})
